4f8344cb356066c8c0940d112fa4a158e042e67a
YuvalFireFly <yuval@gofirefly.io>
2022-03-15 17:29:40 +0200

    Create VPC and EKS cluster

---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "vpc_name" {
+  type    = string
+  default = "terraform-vpc"
+}
+
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnets_azs" {
+  type    = list(string)
+  default = ["us-west-2a", "us-west-2b"]
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "nat_gateways" {
+  type    = bool
+  default = true
+}
+
+variable "security_group_name" {
+  type    = string
+  default = "ssh"
+}
+
+variable "security_group_description" {
+  type    = string
+  default = "Port 22"
+}
+
+variable "ingress_description" {
+  type    = string
+  default = "Allow SSH access"
+}
+
+variable "ingress_protocol" {
+  type    = string
+  default = "tcp"
+}
+
+variable "ingress_from_port" {
+  type    = number
+  default = 22
+}
+
+variable "ingress_to_port" {
+  type    = number
+  default = 22
+}
+
+variable "ingress_cidr_blocks" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "ingress_ipv6_cidr_blocks" {
+  type    = list(string)
+  default = null
+}
